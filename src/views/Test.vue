47c43d9e84f0ddb71325487ba375eee265a0d124
kamil.wozniak <kamil.wozniak@antologic.com>
2020-12-30 19:14:14 +0100

    Add typescript linting

---
+++ src/views/Test.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="about">
+    <h1>This is a test page</h1>
+    <c-hello-world :msg="testMsg"
+                   test="yyyy"/>
+
+  </div>
+</template>
+<script lang="ts">
+import cHelloWorld from '@/components/HelloWorld.vue';
+import { defineComponent } from 'vue';
+
+interface TestMsg {
+  value: string;
+}
+
+export default defineComponent({
+  name: 'Test',
+  components: {
+    cHelloWorld,
+  },
+  setup() {
+    const testMsg: TestMsg = {
+      value: 'testMsg',
+    };
+
+    return {
+      testMsg: testMsg.value,
+    };
+  },
+
+});
+</script>
