bcf0b6e28919757e07e59daacfcb55c3a5c9789b
kamilWozniak <kamilw.wozniak@gmail.com>
2020-12-31 14:12:23 +0100

    Clean up code and add simple unit test

---
+++ src/views/home.view.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="vHome">
+    <p> Home </p>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'vHome',
+  props: {
+    msg: String,
+  },
+});
+</script>
+
+<style scoped
+       lang="scss">
+
+.vHome {
+
+}
+
+</style>
